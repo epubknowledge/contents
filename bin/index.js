99dcacd529552605995ae95e1b894e0daee730ae
codingChewie <codingChewie@gmail.com>
2021-06-22 14:11:33 -0500

    added option argument

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,9 @@
 const parser = require('../src/parser')
+const options = require('../src/options')
 
-const contents = async d => await parser(d)
+const contents = async (dir, args = false) => {
+  const result = await parser(dir)
+  return args === false ? result : await options(result,args)
+}
 
 module.exports = contents
