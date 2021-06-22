862eee6a6b45ef44340c686c23176448acd4baa1
codingChewie <codingChewie@gmail.com>
2021-06-22 15:41:15 -0500

    added multple options param

--- src/modify.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  modDir: async (obj,value) => {
-    let newObj = []
-    await Object.keys(obj).map((key) => obj[key].path.full.toString().toLowerCase().includes(`/${value}/`) && newObj.push(obj[key]))
-    return newObj
-  }
-}
--- src/options.js
+++ src/options.js
@@ -1,55 +1,30 @@
-const {modDir} = require('./modify')
-
 const options = (obj,args) => {
   // console.log('obj', typeof obj === 'object' && obj !== null)
   // console.log('args:',args)
 
   try {
-    return Array.isArray(args) === true ? handleArray(args) : caseOptions(obj,args)
+    return Array.isArray(args) === true ? modMulti(obj,args) : modSingle(obj,args)
   } catch (e) {
     console.log('Error: ', e)
   }
 }
 
-const handleArray = (args) => {
-  let mod = {}
-  
-  args.map((item) => {
-    console.log('item: ',item)
-  })
+const modSingle = async (obj,value) => {
+  let newObj = []
+  await Object.keys(obj).map((key) => obj[key].path.full.toString().toLowerCase().includes(value) && newObj.push(obj[key]))
+  return newObj
 }
 
-const caseOptions = (obj,arg) => {
-  switch (arg.trim().toLowerCase()) {
-    case 'meta':
-        return modDir(obj, 'meta-inf')
-    case 'oebps':
-      return modDir(obj, 'oebps')
-      case 'css':
-      return modDir(obj,'css')
-    case 'font':
-    return modDir(obj,'font')
-case 'img':
-    return modDir(obj,'img')
-    case 'text':
-    return modDir(obj,'text')
 
-
-      // case 'mime':
-    // return modify('mime')
-    // case 'opf':
-    // return modify('opf')
-    // case 'css':
-    // return modify('css')
-    // case 'nav':
-    // return modify('nav')
-    // case 'audio':
-    // return modify('audio')
-    // case 'video':
-    // return modify('video')
-
-    default: return obj
-  }
+const modMulti = async (obj,value) => {
+  let newObj = []
+  await Object.keys(obj).map((key) => {
+    const parsed = obj[key].path.full.toString().toLowerCase()
+    value.map((item) => {
+      parsed.includes(item) && newObj.push(obj[key])
+    })
+  })
+  return newObj
 }
 
 module.exports = options
