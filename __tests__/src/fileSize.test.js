b6954acd06262023fe7bd362f49af026e145b2b3
codingChewie <codingChewie@gmail.com>
2021-06-23 15:54:23 -0500

    added file size tests

---
+++ __tests__/src/fileSize.test.js
@@ -0,0 +1,26 @@
+const path = require('path')
+
+const testDir = require('../setup')
+const fileSize = require('../../src/fileSize')
+
+const badPath = path.resolve(testDir, `!@#$%^&*()`)
+
+test('Test file size of mimetype', async () => {
+  const size = await fileSize(path.resolve(testDir, 'mimetype'))
+  expect(size).toEqual(20)
+})
+
+test('Test with bad file path', async () => {
+  try {
+    const mock = jest.spyOn(process, 'exit').mockImplementation(() => {})
+    await fileSize(badPath)
+    expect(mock).toHaveBeenCalledWith(ERROR_CODE)
+  } catch {}
+})
+
+test(`Test error should console log`, async () => {
+  try {
+    await fileSize(badPath)
+    expect(console.log).toHaveBeenCalledWith('Error: file size error')
+  } catch {}
+})
