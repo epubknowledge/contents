ac06aa6da621c864190c767fcee3d7dc35375cb9
codingChewie <codingChewie@gmail.com>
2021-06-23 14:26:05 -0500

    integrated try catch

--- src/cutter.js
+++ src/cutter.js
@@ -1,9 +1,13 @@
+const chalk = require('chalk')
+const log = console.log
+
 const cutter = (p, fat) => {
   try {
     const trimmed = p.split(fat)
     return trimmed.length === 2 ? trimmed[1] : trimmed[0]
   } catch (e) {
-    console.log('trim error: ', e)
+    log(chalk.red('Error:'), chalk.white('trim error'))
+    process.exit(1)
   }
 }
 
