0667327a1ccda65043eedaf34c07a50dc6600e26
codingChewie <codingChewie@gmail.com>
2021-06-23 15:52:34 -0500

    added ternary for testing size

--- src/fileSize.js
+++ src/fileSize.js
@@ -4,7 +4,8 @@ const log = console.log
 
 const fileSize = f => {
   try {
-    return fs.statSync(f).size
+    const sizer = fs.statSync(f).size
+    return Number.isInteger(s) ? s : new Error()
   } catch (e) {
     log(chalk.red('Error:'), chalk.white('file size error'))
     process.exit(1)
