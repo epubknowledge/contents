b14d84d304e929fc61defa927e0d599d8af3c6c6
codingChewie <codingChewie@gmail.com>
2021-06-23 12:12:38 -0500

    added root level testing

--- __tests__/bin/index.test.js
+++ __tests__/bin/index.test.js
@@ -1,20 +1,9 @@
-// const fs = require('fs')
 const path = require('path')
-
 const testDir = require('../setup')
 const parser = require('../../src/parser')
-
-console.log('md: ', testDir)
 
 test('Test if the directory mobi-dick exists', async () =>
   expect(testDir).toContain('__tests__/moby-dick'))
 
 test('Test path passed to parser returns object', async () =>
   expect(await parser(testDir)).toBeInstanceOf(Object))
-
-test('Test if dir is empty', async () => {
-  const emptyDir = path.resolve(testDir, 'META-INF', 'empty')
-  const parsed = await parser(emptyDir)
-  console.log('empty: ', parsed)
-  // expect(parsed).toBeInstanceOf(Object)
-})
