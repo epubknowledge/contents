f1e1396e8469755791d4bef58b46769a73d50b43
codingChewie <codingChewie@gmail.com>
2021-06-23 15:29:33 -0500

    added more tests

--- __tests__/src/parser.test.js
+++ __tests__/src/parser.test.js
@@ -1,8 +1,22 @@
-const path = require('path')
 const testDir = require('../setup')
 const parser = require('../../src/parser')
 
 test('Test length', async () => {
   const parsed = await parser(testDir)
   expect(parsed.length).toEqual(154)
+})
+
+test(`Bad dir passed`, async () => {
+  try {
+    const mock = jest.spyOn(process, 'exit').mockImplementation(() => {})
+    await parser(['1'])
+    expect(mock).toHaveBeenCalledWith(ERROR_CODE)
+  } catch {}
+})
+
+test(`Should console log error message`, async () => {
+  try {
+    await parser(['1'])
+    expect(console.log).toHaveBeenCalledWith('Error: 1 is not string')
+  } catch {}
 })
