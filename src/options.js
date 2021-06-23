5291a0e66e8f176849b21b90e14452f8553b15f3
codingChewie <codingChewie@gmail.com>
2021-06-23 09:05:56 -0500

    replaced include with regex

--- src/options.js
+++ src/options.js
@@ -10,19 +10,21 @@ const options = (obj, args) => {
 }
 
 const modSingle = async (obj, value) => {
+  const reg = new RegExp(value, 'g')
   let newObj = []
-  await Object.keys(obj).map(
-    key => obj[key].path.full.toString().toLowerCase().includes(value) && newObj.push(obj[key]),
-  )
+  await Object.keys(obj).map(key => {
+    obj[key].path.full.toString().match(reg) && newObj.push(obj[key])
+  })
   return newObj
 }
 
 const modMulti = async (obj, value) => {
   let newObj = []
   await Object.keys(obj).map(key => {
-    const parsed = obj[key].path.full.toString().toLowerCase()
+    const parsed = obj[key].path.full.toString()
     value.map(item => {
-      parsed.includes(item) && newObj.push(obj[key])
+      const reg = new RegExp(item, 'g')
+      parsed.match(item) && newObj.push(obj[key])
     })
   })
   return newObj
