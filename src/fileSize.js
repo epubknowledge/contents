95f6031368bd9313b43438958891be4acaac5f7a
codingChewie <codingChewie@gmail.com>
2021-06-23 15:58:20 -0500

    corrected variable issue

--- src/fileSize.js
+++ src/fileSize.js
@@ -5,7 +5,7 @@ const log = console.log
 const fileSize = f => {
   try {
     const sizer = fs.statSync(f).size
-    return Number.isInteger(s) ? s : new Error()
+    return Number.isInteger(sizer) ? sizer : new Error()
   } catch (e) {
     log(chalk.red('Error:'), chalk.white('file size error'))
     process.exit(1)
