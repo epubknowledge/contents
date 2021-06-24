2d2bdd49a7759e523806fcc47130db26f957d363
codingChewie <codingChewie@gmail.com>
2021-06-24 10:06:32 -0500

    updated with dir check

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,10 @@
+const isDir = require('../src/isDir')
 const parser = require('../src/parser')
 const options = require('../src/options')
 
 const contents = async (dir, args = false) => {
-  const result = await parser(dir)
+  const buildDir = await isDir(dir)
+  const result = await parser(buildDir)
   return args === false ? result : await options(result, args)
 }
 
