0c6cd3d5e0b28f9d26e395570170e35a0fd8c780
codingChewie <codingChewie@gmail.com>
2021-06-24 10:51:39 -0500

    cleaned code

--- src/options.js
+++ src/options.js
@@ -1,27 +1,4 @@
-const options = (obj, args) => {
-  // console.log('obj', typeof obj === 'object' && obj !== null)
-  return Array.isArray(args) === true ? modMulti(obj, args) : modSingle(obj, args)
-}
+const { modSingle, modMulti } = require('./mod')
 
-const modSingle = async (obj, value) => {
-  const reg = new RegExp(value, 'g')
-  let newObj = []
-  await Object.keys(obj).map(key => {
-    obj[key].path.full.toString().match(reg) && newObj.push(obj[key])
-  })
-  return newObj
-}
-
-const modMulti = async (obj, value) => {
-  let newObj = []
-  await Object.keys(obj).map(key => {
-    const parsed = obj[key].path.full.toString()
-    value.map(item => {
-      const reg = new RegExp(item, 'g')
-      parsed.match(item) && newObj.push(obj[key])
-    })
-  })
-  return newObj
-}
-
-module.exports = options
+module.exports = (obj, args) =>
+  Array.isArray(args) === true ? modMulti(obj, args) : modSingle(obj, args)
